a9cf700f80f1d7a1a29e4b41fa297b0abc71b2cf
LorenzoMotelli <lorenzo.motelli@gmail.com>
2021-08-17 10:28:08 +0200

    Some changes in the tree's function, but still doesn't work

--- GraphRanker.c
+++ GraphRanker.c
@@ -4,59 +4,71 @@
 #include <math.h>
 
 #define INF 4294967295
+#define RED 'r'
+#define BLACK 'b'
 
 int graphReadBefore = 0;
 int insertions = 0;
-int width = 0;
 
-struct listNode{
+typedef struct listNode{
     int name;   //the name is based on how many graph are read before
     long long int valueOfThePath; //the sum of minimum path stating from 0 to every other node in graph
-    //char color;
-    struct listNode *bigger;
-    struct listNode *smaller;
-};
-
-typedef struct listNode *node;
+    char color;
+    struct listNode *parent;
+    struct listNode *right;
+    struct listNode *left;
+}node_t;
 
 long long int takeGraph(long int nodes);
 long long int dijkstra(long int source, long int numberOfVertex, long int graph[numberOfVertex][numberOfVertex]);
 long long int sumPaths(long int distancesFrom0[], int range);
-void addGraph(long long int totalSumOfPath, int graphName, int k, struct listNode* headOfTheTree);
-int printTree(struct listNode* root, int isLeft, int offset, int depth, char s[20][255]);
-void printTreeSons(struct listNode* nodeSx, struct listNode* nodeDx);
-void deleteGraph(struct listNode* root);
-void remixing(struct listNode* root);
-void TopK(int k, struct listNode* root);
+node_t *addGraph(node_t **root, node_t *newNode);
+void insertFixup(node_t **root, node_t *node);
+void deleteGraph(node_t **root);
+void deleteFixup(node_t **root, node_t *node);
+void leftRotate(node_t **root,node_t *node);
+void rightRotate(node_t **root,node_t *node);
+void TopK(node_t *root);
 
 
 int main(){
-
     long int numberOfNodes, topGraph;
     char input[8];
-    struct listNode* treeHead = NULL;
+    node_t *treeHead = NULL;
+    node_t *nodeToAdd = malloc(sizeof(node_t));
 
     scanf("%ld,%ld", &numberOfNodes, &topGraph);
 
-    //int arrayOfGraphs[topGraph]; //array in which there will put the graphs
     while(1){
         scanf("%s", input);
         if(0 == strcmp("AggiungiGrafo",input)){
             long long int sum = takeGraph(numberOfNodes);
-            addGraph(sum, graphReadBefore, topGraph, treeHead);
-            if(insertions > numberOfNodes){
-                deleteGraph(treeHead);
-                remixing(treeHead);
+            nodeToAdd->name = graphReadBefore;
+            nodeToAdd->valueOfThePath = sum;
+            nodeToAdd->color = RED;
+            nodeToAdd->left = NULL;
+            nodeToAdd->right = NULL;
+            nodeToAdd->parent = NULL;
+            addGraph(&treeHead, nodeToAdd);
+            graphReadBefore++;
+            insertions++;
+            printf("Graph added\n");
+            insertFixup(&treeHead, nodeToAdd);
+            if(insertions > topGraph){
+                printf("I'm going to delete a graph because we reached the max numebr of graphs required\n");
+                deleteGraph(&treeHead);
             }
         }
-        if(0 == strcmp("TopK",input)) TopK(topGraph, treeHead);
+        if(0 == strcmp("TopK",input)) TopK(treeHead);
     }
+    free(treeHead);
+    free(nodeToAdd);
     return 0;
 }
 
-/*
-* take the graph matrix and invoke the dijkstra algorithm to find the minimum path from the source and the other nodes
-* return lon long int : the value of the path sum
+/**
+ * take the graph matrix and invoke the dijkstra algorithm to find the minimum path from the source and the other nodes
+ * return the value of the paths' sum
 */
 long long int takeGraph(long int nodes){
     long int graphMatrix[nodes][nodes];
@@ -90,6 +102,10 @@ long long int takeGraph(long int nodes){
     return dijkstra(0, nodes, graphMatrix);
 }
 
+/**
+ * dijksta algorithm
+ * return the value of the minimum distance between the source 0 and the other nodes in the graph
+*/
 long long int dijkstra(long int source, long int numberOfVertex, long int graph[numberOfVertex][numberOfVertex]){
     long int costMatrix[numberOfVertex][numberOfVertex];
     long int distance[numberOfVertex]; //distance[i] = shortest distance between source and i
@@ -141,6 +157,9 @@ long long int dijkstra(long int source, long int numberOfVertex, long int graph[
     return sumOfPath;
 }
 
+/**
+ * function to sum the paths
+*/
 long long int sumPaths(long int distancesFrom0[], int range){
     int total = 0;
     for(int i = 0; i < range; i++){
@@ -149,73 +168,239 @@ long long int sumPaths(long int distancesFrom0[], int range){
     return total;
 }
 
-void addGraph(long long int totalSumOfPath, int graphName, int k, struct listNode* headOfTheTree){
-    printf("The graphs since then are: %d\n", graphName); 
-    //adding the new graph in the tree
-    if(0 == insertions){
-        printf("Insertion of the first graph:\n");
-        headOfTheTree = malloc(sizeof(struct listNode));
-        headOfTheTree->name = graphName;
-        headOfTheTree->valueOfThePath = totalSumOfPath;
-        headOfTheTree->bigger = NULL;
-        headOfTheTree->smaller = NULL;
-        printf("Root initalized\n");
+/**
+ * add the graph given by the input
+*/
+node_t *addGraph(node_t **root, node_t *newNode){
+    printf("I'm in addGraph\n");
+    if(NULL == (*root)){
+        printf("Root is NULL\n");
+        (*root) = newNode;
+        return (*root);
     }
-
+    if(newNode->valueOfThePath < (*root)->valueOfThePath){
+        printf("Go left\n");
+        (*root)->left = addGraph(&(*root)->left, newNode);
+        (*root)->left->parent = (*root);
+        printf("New node parent:%d\n", (*root)->parent->name);
+    }
     else{
-        printf("Insertion of the %d graph\n", graphName);
-        struct listNode* node = malloc(sizeof(struct listNode));
-        struct listNode* currentNode = malloc(sizeof(struct listNode));
+        printf("Go right\n");
+        (*root)->right = addGraph(&(*root)->right, newNode);
+        (*root)->right->parent = (*root);
+        printf("New node parent:%d\n", (*root)->parent->name);
+    }
+    return (*root);
+}
 
-        node->name = graphName;
-        node->valueOfThePath = totalSumOfPath;
-        node->bigger = NULL;
-        node->smaller = NULL;
-
-        currentNode  = headOfTheTree;
-        while(currentNode != NULL){
-            if(node->valueOfThePath < currentNode->valueOfThePath){
-                currentNode = currentNode->smaller;
-            }
-            else{
-                currentNode = currentNode->bigger;
+/**
+ * function to balance the tree after an insertion of a node
+*/
+void insertFixup(node_t **root, node_t *node){
+    printf("I'm in the insert fixup function\n");
+    while(node != (*root) && node->parent->color == RED){
+        printf("I'm in the while loop\n");
+        node_t *x = node->parent;
+        if(RED == x->color){
+            printf("X is red\n");
+            if(x == x->parent->left){
+                printf("Initilization of y\n");
+                node_t *y = x->parent->right;
+                if(RED == y->color){
+                    printf("Change colors\n");
+                    x->color = BLACK;
+                    y->color = BLACK;
+                    x->parent->color = RED;
+                    node = x->parent;
+                }
+                else{
+                    printf("Need a left rotation\n");
+                    if(node == x->right){
+                        node = x;
+                        leftRotate(root, node);
+                        x = node->parent;
+                    }
+                    printf("Change color and do a right rotation\n");
+                    x->color = BLACK;
+                    x->parent->color = RED;
+                    rightRotate(root, x->parent);
+                }
             }
         }
-        currentNode->name = graphName;
-        currentNode->valueOfThePath = node->valueOfThePath;
-        currentNode->bigger = NULL;
-        currentNode->smaller = NULL;
+        else{
+            printf("Initialization of y\n");
+            node_t *y = x->parent->left;
+            if(RED == y->color){
+                printf("Y was red, so change of colors\n");
+                x->color = BLACK;
+                y->color = BLACK;
+                x->parent->color = RED;
+                node = x->parent;
+            }
+            else{
+                printf("Y is black\n");
+                if(node == x->left){
+                    printf("Need a right rotation\n");
+                    node = x;
+                    rightRotate(root, node);
+                    x = node->parent;
+                }
+                printf("Change colors and do a left rotation\n");
+                x->color = BLACK;
+                x->parent->color = RED;
+                leftRotate(root, x->parent);
+            }
+        }
     }
-
-    graphReadBefore++;
-    insertions++;
-    width++;
-    
-    return;
+    printf("The root color is now black\n");
+    (*root)->color = BLACK;
 }
 
-void deleteGraph(struct listNode* root){
-    struct listNode* currentNode = malloc(sizeof(struct listNode));
-    currentNode = root;
-    while(NULL != currentNode->bigger){
-        currentNode = currentNode->bigger;
+/**
+ * delete the node (graph) with the maximum path distance
+ * (the node at the most right in the tree)
+*/
+void deleteGraph(node_t **root){
+    node_t *currentNode = malloc(sizeof(node_t));
+    node_t *x = NULL;
+
+    printf("I'm in the fuction deleteGraph\n");
+    currentNode = *root;
+    printf("currentNode initialized\n");
+    while(NULL != currentNode->right && NULL != currentNode->left){
+        printf("I'm searching the node to delete:\n");
+        if(currentNode->right->valueOfThePath >= currentNode->valueOfThePath){
+            printf("The greater graph is currentNode->right\n");
+            currentNode = currentNode->right;
+        }
+        else{
+            printf("The greater graph is currentNode->right\n");
+            currentNode = currentNode->left;
+        }
     }
-    if(NULL != currentNode->smaller){
-        currentNode = currentNode->smaller;
-    }
+    x = currentNode->parent;
     free(currentNode);
+    deleteFixup(root, x);
+
     return;
 }
 
-void remixing(struct listNode* root){
+/**
+ * function to balance the tree after an erasure of a node
+*/
+void deleteFixup(node_t **root, node_t *node){
+    while (node != *root && node->color == BLACK){
+		if (node == node->parent->left){
+			node_t *x = node->parent->right;
+			if (x->color == RED){
+				x->color = BLACK;
+				node->parent->color = RED;
+				leftRotate(root, node->parent);
+				x = node->parent->right;
+			}
+			if (x->left->color == BLACK && x->right->color == BLACK){
+				x->color = RED;
+				node = node->parent;
+			}
+			else{
+				if (x->right->color == BLACK){
+					x->left->color = BLACK;
+					x->color = RED;
+					rightRotate(root, x);
+					x = node->parent->right;
+				}
+				x->color = node->parent->color;
+				node->parent->color = BLACK;
+				x->right->color = BLACK;
+				leftRotate(root, node->parent);
+				node = *root;
+			}
+		}
+		else{
+			node_t *x = node->parent->left;
+			if (x->color == RED){
+				x->color = BLACK;
+				node->parent->color = RED;
+				rightRotate(root, node->parent);
+				x = node->parent->left;
+			}
+			if (x->right->color == BLACK && x->left->color == BLACK){
+				x->color = RED;
+				node = node->parent;
+			}
+			else{
+				if (x->left->color == BLACK){
+					x->right->color = BLACK;
+					x->color = RED;
+					leftRotate(root, x);
+					x = node->parent->left;
+				}
+				x->color = node->parent->color;
+				node->parent->color = BLACK;
+				x->left->color = BLACK;
+				rightRotate(root, node->parent);
+				node = *root;
+			}
+		}
+	}
+	node->color = BLACK;
     return;
 }
 
-//inorder print
-void TopK(int k, struct listNode* root){
-    if(NULL != root){
-        TopK(k, root->smaller);
-        printf("%d", root->name);
-        TopK(k, root->bigger);
+/**
+ * Right and Left rotation of the nodes in the tree to balance it after an insertion or an erasure
+*/
+
+void leftRotate(node_t **root, node_t *node){
+    node_t *rightNode = node->right;
+    node->right = rightNode->left;
+    if(NULL != rightNode){
+        rightNode->right->parent = rightNode;
     }
+    rightNode->parent = node->parent;
+    if(NULL == node->parent){
+        *root = rightNode;
+    }
+    else if(node == node->parent->right){
+        node->parent->right = rightNode;
+    }
+    else{
+        node->parent->left = rightNode;
+    }
+    rightNode->left = node;
+    rightNode->right = rightNode;
+}
+
+void rightRotate(node_t **root, node_t *node){
+    node_t *leftNode = node->left;
+    node->left = leftNode->right;
+    if(NULL != leftNode){
+        leftNode->left->parent = leftNode;
+    }
+    leftNode->parent = node->parent;
+    if(NULL == node->parent){
+        *root = leftNode;
+    }
+    else if(node == node->parent->left){
+        node->parent->left = leftNode;
+    }
+    else{
+        node->parent->right = leftNode;
+    }
+    leftNode->right = node;
+    leftNode->left = leftNode;
+}
+
+/**
+ * inorder print names of the graphs
+*/
+void TopK(node_t *root){
+    printf("I'm in TopK\n");
+    if(NULL == root){
+        printf("The root is NULL, exiting the function\n");
+        return;
+    }
+    TopK(root->left);
+    printf("%d ", root->name);
+    TopK(root->right);
 }
